414016b589c3ed116d206e16806e693220491dea
Matt <mdnebel@gmail.com>
2025-10-03 19:35:29 -0700

    Max upper bound test less precise

--- csharp/Sieve.Tests/SieveTests.cs
+++ csharp/Sieve.Tests/SieveTests.cs
@@ -34,22 +34,26 @@ namespace Sieve.Tests
         }
 
         [TestMethod]
-        [DataRow(0, 37)] // actual prime: 2
-        [DataRow(1, 37)] // actual prime: 3
-        [DataRow(2, 37)] // actual prime: 5
-        [DataRow(11, 37)] // actual prime: 37
-        [DataRow(12, 41)] // actual prime: 41
-        [DataRow(19, 75)] // actual prime: 71
-        [DataRow(99, 572)] // actual prime: 541
-        [DataRow(500, 3794)] // actual prime: 3581
-        [DataRow(986, 8221)] // actual prime: 7793
-        [DataRow(2000, 18229)] // actual prime: 17393
-        [DataRow(1000000, 15783429)] // actual prime: 15485867
-        [DataRow(10000000, 182084922)] // actual prime: 179424691
-        [DataRow(100000000, 2061884872)] // actual prime: 2038074751
-        public void TestNthUpperBound(long n, long expectedUpperBound)
+        [DataRow(0, 2)]
+        [DataRow(1, 3)]
+        [DataRow(2, 5)]
+        [DataRow(11, 37)]
+        [DataRow(12, 41)]
+        [DataRow(19, 71)]
+        [DataRow(99, 541)]
+        [DataRow(500, 3581)]
+        [DataRow(986, 7793)]
+        [DataRow(2000, 17393)]
+        [DataRow(1000000, 15485867)]
+        [DataRow(10000000, 179424691)]
+        [DataRow(100000000, 2038074751)]
+        public void TestNthUpperBound(long n, long prime)
         {
-            Assert.AreEqual(expectedUpperBound, SieveImplementation.GetUpperBound(n));
+            long upperBound = SieveImplementation.GetUpperBound(n);
+            Assert.IsTrue(upperBound >= prime, $"Expected upper bound {upperBound} for n {n} to be greater than prime {prime}");
+            long error = upperBound - prime;
+            var maxError = (long)(prime * 0.1) + 37; // An upper bound of 37 is expected for any n below 12
+            Assert.IsTrue(error <= maxError, $"Expected upper bound {upperBound} for n {n} to be no more than 10% larger than its associated prime {prime}");
         }
 
         [TestMethod]
